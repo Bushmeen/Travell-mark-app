3d115d90df6df72e851b0c20d681e1c137fd0662
Jakub Sepetowski <bushmeen@gmail.com>
2023-01-18 19:45:23 +0100

    change

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+const key=config.API_KEY;
 const body = document.querySelector('body');
 const filterBtn = document.querySelector('.sidebar__filer');
 const sideBarOptions = document.querySelector('.sidebar__options');
@@ -118,7 +119,7 @@ class App {
 
 		this.#map = L.map('map').setView(this.#cords, this.#mapZoomLevel);
 		L.tileLayer(
-			'https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png?api_key=71cd2d43-0a48-4641-abbc-cc4993c56965',
+			`https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png?api_key=${key}`,
 			{
 				maxZoom: 20,
 				attribution:
